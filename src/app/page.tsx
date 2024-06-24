49ca34c188b7a04bdadf21a34e8a53cab1a32cce
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-25 01:02:29 +0530

    chore: Update dependencies and remove unused SVG files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,109 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
-      <section>
-        <MaxWidthWrapper 
-        className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-center">
-              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-              <Image 
-              src="/snake-1.png"
-              alt="Snake"
-              width={500}
-              height={500}
-              />
+    
+    <section>
+        <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+          <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
+            <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
+              <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
+                {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
+                <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
+                <img src='/snake-1.png' className='w-full' alt='Decorative snake' />
               </div>
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your Image on a <span className="bg-green-600 px-2">Custom</span> Phone Case
-                </h1>
-                <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
-                  Capture your favorite memories with your own,{' '}<span className="font-semibold">one-of-one</span> phone case. CaseCobra allows you to protext your memories,
-                  not just your phone case.
-                </p>
+              <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
+                Your Image on a{' '}
+                <span className='bg-green-600 px-2 text-white'>Custom</span>{' '}
+                Phone Case
+              </h1>
+              <p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
+                Capture your favorite memories with your own,{' '}
+                <span className='font-semibold'>one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
 
-                <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
-                  <div className="space-y-2">
-                    <li className="flex gap-1.5 items-center text-left">
-                     <Check className="h-5 w-5 shrink-0 text-green-600" />
-                      High-quality, durable material
-                    </li>
-                    <li className="flex gap-1.5 items-center text-left">
-                     <Check className="h-5 w-5 shrink-0 text-green-600" />
-                      5 year pring guarantee
-                    </li>
-                    <li className="flex gap-1.5 items-center text-left">
-                     <Check className="h-5 w-5 shrink-0 text-green-600" />
-                     Modern iPhone models supported
-                    </li>
-                  </div>
-                </ul>
-
-                <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
-                  <div className="flex -space-x-4">
-                    <Image 
-                    src="/users/user-1.png"
-                    alt="User 1"
-                    width={500}
-                    height={500}
-                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
-                    />
-                    <Image 
-                    src="/users/user-2.png"
-                    alt="User 2"
-                    width={500}
-                    height={500}
-                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
-                    />
-                    <Image 
-                    src="/users/user-3.png"
-                    alt="User 3"
-                    width={500}
-                    height={500}
-                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
-                    />
-                    <Image 
-                    src="/users/user-4.jpg"
-                    alt="User 4"
-                    width={500}
-                    height={500}
-                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
-                    />
-                    <Image 
-                    src="/users/user-5.jpg"
-                    alt="User 5"
-                    width={500}
-                    height={500}
-                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-hover"
-                    />
-                  </div>
-
-                  <div className="flex flex-col justify-between items-center sm:items-start">
-                    <div className="flex gap-0.5">
-                      <Star className="g-4 w-4 text-green-600 fill-green-600" />
-                      <Star className="g-4 w-4 text-green-600 fill-green-600" />
-                      <Star className="g-4 w-4 text-green-600 fill-green-600" />
-                      <Star className="g-4 w-4 text-green-600 fill-green-600" />
-                      <Star className="g-4 w-4 text-green-600 fill-green-600" />
-                    </div>
-
-                    <p><span className="font-semibold">1.250</span> happy customers</p>
-                  </div>
+              <ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
+                <div className='space-y-2'>
+                  <li className='flex gap-1.5 items-center text-left'>
+                    <Check className='h-5 w-5 shrink-0 text-green-600' />
+                    High-quality, durable material
+                  </li>
+                  <li className='flex gap-1.5 items-center text-left'>
+                    <Check className='h-5 w-5 shrink-0 text-green-600' />5 year
+                    print guarantee
+                  </li>
+                  <li className='flex gap-1.5 items-center text-left'>
+                    <Check className='h-5 w-5 shrink-0 text-green-600' />
+                    Modern iPhone models supported
+                  </li>
                 </div>
+              </ul>
+
+              <div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
+                <div className='flex -space-x-4'>
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-1.png'
+                    alt='User 1'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-2.png'
+                    alt='User 2'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-3.png'
+                    alt='User 3'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-4.jpg'
+                    alt='User 4'
+                  />
+                  <img
+                    className='inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-5.jpg'
+                    alt='User 5'
+                  />
+                </div>
+
+                <div className='flex flex-col justify-between items-center sm:items-start'>
+                  <div className='flex gap-0.5'>
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                  </div>
+
+                  <p>
+                    <span className='font-semibold'>1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
+            <div className='relative md:max-w-xl'>
+              <img
+                src='/your-image.png'
+                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+                alt='Your custom image'
+              />
+              <img
+                src='/line.png'
+                className='absolute w-20 -left-6 -bottom-6 select-none'
+                alt='Decorative line'
+              />
+              <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
             </div>
           </div>
         </MaxWidthWrapper>
